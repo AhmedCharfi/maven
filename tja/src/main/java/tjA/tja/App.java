97ed44cf6368a9bdb6bb1e606589e32aa662081a
Ahmed Charfi <hamdouchcharfi@yahoo.fr>
2015-12-11 23:12:25 +0100

    1 commit

---
+++ tja/src/main/java/tjA/tja/App.java
@@ -0,0 +1,13 @@
+package tjA.tja;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
